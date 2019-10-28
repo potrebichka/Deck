f629f9ccdfe6c3a091ff76ad0602cfed518f5198
Nina Potrebich and Trevor Mackin <trevor@>
2019-10-28 15:38:04 -0700

    begin playing cards project

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
+  var suits = ['clubs', 'hearts', 'diamonds', 'spades'];
 
+  var values = ['ace'];
+  for (var i=2; i<=10; i++) {
+    values.push(i)
+  };
+  values.push('jack', 'queen', 'king');
 
+  var resultArray = [];
+
+  suits.forEach(function(suit) {
+    values.forEach(function(value) {
+      var result = value + " of " + suit;
+      resultArray.push(result);
+      $("#list").append("<li>" + result + "</li>");
+    });
+  });
 
 });
